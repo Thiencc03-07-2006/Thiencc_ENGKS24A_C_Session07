c4cba899fce7c389d293f5b0307bb3757c8ccc8e
Thiencc03-07-2006 <cct20062250.@gmail.com>
2024-11-28 02:12:44 +0700

    Update session07-3.c

--- session07-3.c
+++ session07-3.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	int arr[5]={1,2,3,4,5},i=0,chan=1;
+	int arr[5]={1,2,3,4,5},i=0,even=1;
 	while(i<sizeof(arr)/sizeof(arr[0])){
 		if(arr[i]%2==0){
-			printf("so chan %d\n",arr[i]);
-			chan=0;
+			printf("so even %d\n",arr[i]);
+			even=0;
 		}
 		++i;
 	}
-	if(chan){
-			printf(" ko co so chan\n");
+	if(even){
+			printf(" ko co so even\n");
 		}
 	return 0;
 }
